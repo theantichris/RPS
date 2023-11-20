fabf15e526febd2d48b346f4e9f3da6cfbd7549d
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-20 11:01:08 -0500

    added game state and method to return the results

---
+++ RPS/GameState.swift
@@ -0,0 +1,12 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Christopher Lamm on 11/20/23.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
--- RPS/Sign.swift
+++ RPS/Sign.swift
@@ -16,8 +16,53 @@ enum Sign {
             return "👊"
         case .paper:
             return "✋"
-        default:
+        case .scissors:
             return "✌️"
         }
+    }
+    
+    func gameResults(for opponentSign: Sign) -> GameState {
+        var gameState: GameState = .draw
+        
+        switch self {
+        case .rock:
+            if opponentSign == .rock {
+                gameState = .draw
+            }
+            
+            if opponentSign == .paper {
+                gameState = .lose
+            }
+            
+            if opponentSign == .scissors {
+                gameState = .win
+            }
+        case .paper:
+            if opponentSign == .rock {
+                gameState = .win
+            }
+            
+            if opponentSign == .paper {
+                gameState = .draw
+            }
+            
+            if opponentSign == .scissors {
+                gameState = .lose
+            }
+        case .scissors:
+            if opponentSign == .rock {
+                gameState = .lose
+            }
+            
+            if opponentSign == .paper {
+                gameState = .win
+            }
+            
+            if opponentSign == .scissors {
+                gameState = .draw
+            }
+        }
+        
+        return gameState
     }
 }
