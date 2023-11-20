dfc550cbca583c239a0d4bb403c0233efdc6cdd4
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-20 11:04:58 -0500

    added method to get a random sign, renamed emoji property

--- RPS/Sign.swift
+++ RPS/Sign.swift
@@ -7,10 +7,22 @@
 
 import Foundation
 
+func randomSign() -> Sign {
+    let sign = Int.random(in: 0...2)
+    
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
+
 enum Sign {
     case rock, paper, scissors
     
-    var getSign: String {
+    var getEmoji: String {
         switch self {
         case .rock:
             return "👊"
