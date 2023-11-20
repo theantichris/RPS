2d79e9f6e6f9e6fb63b8793a5a8b6dd3e6d61e3e
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-20 11:31:14 -0500

    added welcome message

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        statusLabel.text = "Choose your weapon!"
     }
 
 
