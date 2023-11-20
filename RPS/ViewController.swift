9da93f5a2b55fa9aae05c5bea4792832eb67ac31
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-20 13:27:47 -0500

    added the game start state

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -11,6 +11,11 @@ class ViewController: UIViewController {
     var gameState: GameState = .start
 
     @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var computerSign: UILabel!
+    @IBOutlet weak var playAgain: UIButton!
+    @IBOutlet weak var rock: UIButton!
+    @IBOutlet weak var paper: UIButton!
+    @IBOutlet weak var scissors: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +27,19 @@ class ViewController: UIViewController {
         switch gameState {
         case .start:
             view.backgroundColor = UIColor.white
+            computerSign.text = "🤖"
             statusLabel.text = gameState.message
+            
+            playAgain.isHidden = true
+            
+            rock.isHidden = false
+            rock.isEnabled = true
+            
+            paper.isHidden = false
+            paper.isEnabled = true
+            
+            scissors.isHidden = false
+            scissors.isEnabled = true
         case .win:
             view.backgroundColor = UIColor.green
         case .lose:
